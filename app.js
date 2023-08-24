63fb149c8f6640e2930d09c72c95b4f993eef412
Guilherme Alvarenga <guilherme.azevedo@pdcase.com.br>
2023-08-24 15:07:46 -0300

    Commit Fix

--- app.js
+++ app.js
@@ -1,41 +1,31 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express')
+const app = express()
+const mysql = require('mysql2');
+const port = 3005
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+var connection = mysql.createConnection({
+  host     : 'aulascefet.c8tuthxylqic.sa-east-1.rds.amazonaws.com',
+  user     : 'aluno',
+  password : 'alunoc3f3t',
+  database : 'aula_web'
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+connection.connect();
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get('/', function(req, res, next) {
+  res.send("Backend Guilherme Alvarenga de Azevedo Rodando...");
 });
 
-module.exports = app;
+app.get('/cliente', function(req, res, next) {
+  connection.query(
+    'select * from cliente',
+    (err, results, fields) => {
+      if(err) console.log(err)
+      res.send(results)
+    }
+  );
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
