6cbea8d8fc533ae90715acd489ab4f569158360e
Guilherme Alvarenga <guilherme.azevedo@pdcase.com.br>
2023-09-01 11:44:37 -0300

    ATIVIDADE CRUD - FORNECEDORES

--- app.js
+++ app.js
@@ -73,23 +73,22 @@ app.post('/clientes', function(req, res, next) {
   var sql = `insert into cliente(nome, sobrenome,` +
   `email, data_cadastro, salario) values ("${nome}", "${sobrenome}", ` +
   `"${email}", "${moment().format("YYYY-MM-DD")}", ${salario})`
-  // if(nome != undefined && sobrenome != undefined && email != undefined && salario != undefined){
-    connection.query(
-      sql, (erro, resultados, fields) => {
-        if(erro)
-          res.send(erro)
+  
+  connection.query(
+    sql, (erro, resultados, fields) => {
+      if(erro)
+        res.send(erro)
 
-        var caminhoTemp = req.files.avatar.path;
-        var type = req.files.avatar.type.split('/');
-        var caminhoNovo = `./uploads/clientes/cliente${resultados.insertId}.${type[type.length - 1]}`;
+      var caminhoTemp = req.files.avatar.path;
+      var type = req.files.avatar.type.split('/');
+      var caminhoNovo = `./uploads/clientes/cliente${resultados.insertId}.${type[type.length - 1]}`;
 
-        fs.copyFile(caminhoTemp, caminhoNovo, (err) => {
-          console.log(err)
-          res.send(resultados)
-        });
-      }
-    )
-  // }
+      fs.copyFile(caminhoTemp, caminhoNovo, (err) => {
+        console.log(err)
+        res.send(resultados)
+      });
+    }
+  )
 });
 
 app.post('/clientes_del/:id_cliente', function(req, res, next) {
@@ -120,6 +119,119 @@ app.patch('/clientes/:id_cliente', function(req, res, next) {
       res.send(resultados)
     }
   )
+});
+
+
+app.get('/fornecedores', function (req, res, next) {
+  connection.query(
+      'select * from fornecedor',
+      (err, results, fields) => {
+          if (err)
+              console.log(err)
+          res.send(results)
+      }
+  );
+});
+
+app.get('/fornecedores/:id_fornecedor', function (req, res, next) {
+  var idFornecedor = req.params['id_fornecedor'];
+  connection.query(
+      `select * from fornecedor where id_fornecedor = ${+idFornecedor}`,
+      (err, results, fields) => {
+          if (err)
+              console.log(err)
+          res.send(results)
+      }
+  );
+});
+
+app.post('/fornecedores', function (req, res, next) {
+  var razao = req.body.razao;
+
+  var cpf_cnpj = req.body.cpf_cnpj;
+  var contato = req.body.contato;
+  var logradouro = req.body.logradouro;
+  var cidade = req.body.cidade;
+  var uf = req.body.uf;
+  if(razao.length <= 100 && razao != undefined 
+          && cpf_cnpj.length <= 45 && cpf_cnpj != undefined 
+          && contato.length <= 45 && contato != undefined 
+          && uf.length <= 2 && cidade.length <= 45 
+          && logradouro.length <= 100){
+      
+      var sql = `insert into fornecedor(razao, cpf_cnpj,` +
+      `contato, logradouro, cidade, uf) values ("${razao}", "${cpf_cnpj}", ` +
+      `"${contato}", "${logradouro}", "${cidade}", "${uf}")`
+
+      connection.query(
+          sql, (erro, resultados, fields) => {
+              if (erro)
+                  res.send(erro)
+
+              var caminhoTemp = req.files.logomarca.path;
+              var type = req.files.logomarca.type.split('/');
+              var caminhoNovo = `./uploads/fornecedores/F${resultados.insertId}.${type[type.length - 1]}`;
+
+              fs.copyFile(caminhoTemp, caminhoNovo, (err) => {
+                  console.log(err)
+                  res.send(resultados)
+              });
+          }
+      )
+  } else {
+      res.send("Preencha os campos corretamente!");
+  }
+  
+});
+
+app.post('/fornecedores_del/:id_fornecedor', function (req, res, next) {
+  var idFornecedor = req.params['id_fornecedor'];
+  connection.query(
+      `delete from fornecedor where id_fornecedor = ${+idFornecedor}`,
+      (err, results, fields) => {
+          if (err)
+              console.log(err)
+          res.send(results)
+      }
+  );
+});
+
+app.post('/fornecedores/:id_fornecedor', function (req, res, next) {
+  var razao = req.body.razao;
+  var cpf_cnpj = req.body.cpf_cnpj;
+  var contato = req.body.contato;
+  var logradouro = req.body.logradouro;
+  var cidade = req.body.cidade;
+  var uf = req.body.uf;
+  var idFornecedor = req.params['id_fornecedor'];
+
+  if(razao.length <= 100 && razao != undefined 
+    && cpf_cnpj.length <= 45 && cpf_cnpj != undefined 
+    && contato.length <= 45 && contato != undefined 
+    && uf.length <= 2 && cidade.length <= 45 
+    && logradouro.length <= 100){
+
+      var sql = `
+      update fornecedor set razao = "${razao}", cpf_cnpj = "${cpf_cnpj}", ` +
+          `contato = "${contato}", logradouro = "${logradouro}", cidade = "${cidade}", uf = "${uf}" where id_fornecedor = ${idFornecedor}`
+      connection.query(
+          sql, (erro, resultados, fields) => {
+              if (erro)
+                  res.send(erro)
+
+              var caminhoTemp = req.files.logomarca.path;
+              var type = req.files.logomarca.type.split('/');
+              var caminhoNovo = `./uploads/fornecedores/F${idFornecedor}.${type[type.length - 1]}`;
+
+              fs.copyFile(caminhoTemp, caminhoNovo, (err) => {
+                  console.log(err)
+                  res.send(resultados)
+              });
+          }
+      )
+    } else {
+      res.send("Preencha os campos corretamente!");
+    }
 });
 
 app.listen(port, () => {
